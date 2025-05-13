d985fa92dc424a96557037b870a424e549da11a1
tiwaryramesh95@gmail.com <tiwary705014@gmail.com>
2025-05-13 20:49:53 -0300

    add new files

---
+++ math.js
@@ -0,0 +1,12 @@
+// const sum = (a,b) => a+b;
+// const mul = (a,b) => a*b;
+// const g = 9.8;
+// const PI = 3.13;
+
+// module.exports = 123;
+
+const someValue = require("./math");
+
+console.log(someValue);
+
+
