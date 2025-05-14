1ebcda1002028cff437b771db114cffba3605e9a
tiwaryramesh95@gmail.com <tiwary705014@gmail.com>
2025-05-14 13:28:57 -0300

    update new files

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let n = 5;
+// let n = 5;
 
-for (let i = 0; i < n; i++) {
-    console.log("hello, ", i);
-}
+// for (let i = 0; i < n; i++) {
+//     console.log("hello, ", i);
+// }
 
-console.log("bye!");
-console.log("Hello from script.js!");
-console.log("Hello from script.js!");
+// console.log("bye!");
+// console.log("Hello from script.js!");
+// console.log("Hello from script.js!");
+
+ // script.js
+const math = require("./math");
+
+console.log(math);              // Should print the object with sum, mul, g, PI
+console.log(math.sum(2, 2));    // Should print: 4
+
+
