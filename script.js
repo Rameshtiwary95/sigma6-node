e8d031a6ca0c6e2d9512fb915b6426becfd659fe
tiwaryramesh95@gmail.com <tiwary705014@gmail.com>
2025-05-17 13:13:43 -0300

    update new files

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-// const info = require("./Fruits");
+// // const info = require("./Fruits");
 
-// console.log(info[0].name);              // Should print the object with sum, mul, g, PI
+// // console.log(info[0].name);              // Should print the object with sum, mul, g, PI
 
 
-const figlet = require("figlet");
+// const figlet = require("figlet");
 
-figlet("By Ramesh Tiwary", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
-});
+// figlet("By Ramesh Tiwary", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+import {sum,PI} from "./math.js";
+import { generate} from "random-words";
+// console.log(sum(PI));
+console.log (generate());
